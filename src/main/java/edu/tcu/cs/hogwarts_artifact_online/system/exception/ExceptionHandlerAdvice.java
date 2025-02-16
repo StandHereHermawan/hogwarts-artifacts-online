a80cc223e9ea2176d242e685e1a81962100bc81f
Arief-Karditya <nolifegansaya@gmail.com>
2025-02-16 14:34:44 +0700

    And Method Handle Not Found Endpoints

--- src/main/java/edu/tcu/cs/hogwarts_artifact_online/system/exception/ExceptionHandlerAdvice.java
+++ src/main/java/edu/tcu/cs/hogwarts_artifact_online/system/exception/ExceptionHandlerAdvice.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.HashMap;
 import java.util.List;
@@ -73,6 +74,16 @@ public class ExceptionHandlerAdvice {
                 false,
                 StatusCode.FORBIDDEN,
                 "No permission.",
+                exception.getMessage());
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    Result handleNoHandlerFoundException(NoHandlerFoundException exception) {
+        return new Result(
+                false,
+                StatusCode.NOT_FOUND,
+                "This API endpoint is not found.",
                 exception.getMessage());
     }
 
